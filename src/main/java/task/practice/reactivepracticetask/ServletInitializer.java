adf5f5d93711c33ab0ab5747ae041da0ae687a43
홍준성 <powerstar13@hanmail.net>
2022-04-14 22:00:17 +0900

    git init > New Project > Reactive 실습 과제

---
+++ src/main/java/task/practice/reactivepracticetask/ServletInitializer.java
@@ -0,0 +1,13 @@
+package task.practice.reactivepracticetask;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ReactivePracticeTaskApplication.class);
+    }
+
+}
---
+++ src/test/java/task/practice/reactivepracticetask/ReactivePracticeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package task.practice.reactivepracticetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivePracticeTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
